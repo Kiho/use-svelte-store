fd62a973791f59a83e812e047e3a51531875a8f1
Kiho <kihoc22@gmail.com>
2021-05-11 19:49:00 -0700

    Build cjs

--- dist/svelte-store.d.ts
+++
@@ -1,7 +0,0 @@
-import { Readable, Writable, readable, writable } from 'svelte/store';
-type Setter<T> = (v: T) => void;
-type UpdateFn<T> = (v: T) => T;
-type Updater<T> = (u: UpdateFn<T>) => void;
-declare function useReadable<T>(store: Readable<T>): T;
-declare function useWritable<T>(store: Writable<T>): [T, Setter<T>, Updater<T>];
-export { Setter, UpdateFn, Updater, useReadable, useWritable, readable, writable };
--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,8 @@ import typescript from '@wessberg/rollup-plugin-ts';
 export default {
   input: 'src/index.ts',
   output: [
-    { file: `dist/svelte-store.js`, format: 'es' },
+    { file: `es6/svelte-store.js`, format: 'es' },
+    { file: `cjs/svelte-store.js`, format: 'cjs' },
   ],
   external: [
     'react',
